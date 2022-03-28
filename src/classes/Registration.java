fb0fb9f6c4e2078291ab9815d9d9b16e4d1ede84
manzurayusup <manzurayusup@gmail.com>
2022-03-28 13:31:11 -0500

    added relative path

--- src/classes/Registration.java
+++ src/classes/Registration.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.Scanner;
 import java.util.Vector;
 
+
 public class Registration
 {
 	private static Scanner keyboard=new Scanner(System.in);
@@ -16,22 +17,13 @@ public class Registration
     public static void main(String[] args) throws IOException
     {
     	
-        Course data_structures = new Course("Data Structures and Algorithms", "CSE247", "2022-08-29T13:00:00.000Z", "2022-12-09T14:20:00.000Z", 70, 3);
-        map.put("CSE247", data_structures);
-        Course intro_to_cs = new Course("Introduction to Computer Science", "CSE131", "2022-08-29T13:00:00.000Z", "2022-12-09T14:20:00.000Z", 70, 3);
-        Course intro_to_ce = new Course("Introduction to Computer Engineering", "CSE132", "2022-08-29T13:00:00.000Z", "2022-12-09T14:20:00.000Z", 70, 3);
-        map.put("CSE131", intro_to_cs);
-        map.put("CSE132", intro_to_ce);
-        Course rapid = new Course("Rapid Prototype Development and Creative Programming", "CSE330", "2022-08-29T13:00:00.000Z", "2022-12-09T14:20:00.000Z", 70, 3);
-        map.put("CSE330", rapid);
         System.out.println("Welcome to the student registration system!");
         Student currentStudent = logIn();
         while (currentStudent == null) {
         	System.out.println("Did not recognize ID, please try again");
         	currentStudent = logIn(); 	
         }
-//        System.out.println(currentStudent);
-//        System.out.println(currentStudent.getRegisteredCourses());
+
         int choice=5;
         do {
             printChoices();
@@ -49,7 +41,7 @@ public class Registration
             System.out.println(currentStudent.getRegisteredCourses());
         }while (choice!=5);
         saveRegistration();
-        String pathToFileStudents = "/Users/khushi/git/student_registration/src/textfiles/students.txt";
+        String pathToFileStudents = "src/textfiles/students.txt";
 //        String pathToFileStudents = "./../textfiles/students.txt";
 
         
@@ -61,7 +53,7 @@ public class Registration
     		return null;
     	}
     	String stringId = String.valueOf(id);
-    	Student loggedUser = populateStudentObjectFromFile("/Users/khushi/git/student_registration/src/textfiles/students.txt" , stringId);
+    	Student loggedUser = populateStudentObjectFromFile("src/textfiles/students.txt" , stringId);
     	return loggedUser;
     }
     
@@ -141,9 +133,11 @@ public class Registration
     	System.out.println("never going into the if statement");
     	return null;
     }
+    
+
 	
 	public static Course makeCourse(String courseCode) throws FileNotFoundException {
-		String path = "/Users/khushi/git/student_registration/src/textfiles/courses.txt";
+		String path = "src/textfiles/courses.txt";
 		File file = new File(path);
 		Scanner courseReader = new Scanner(file);
 		
